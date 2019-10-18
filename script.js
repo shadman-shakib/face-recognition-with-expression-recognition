a318b64ac599e05496c9b0b1e4b00a62c89fb48b
Ruhul <ruhul20@gmail.com>
2019-10-19 00:47:37 +0600

    initial

---
+++ script.js
@@ -0,0 +1,64 @@
+const imageUpload = document.getElementById('imageUpload')
+
+Promise.all([
+  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+  faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
+  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/models')
+]).then(start)
+
+async function start() {
+  const container = document.createElement('div')
+  container.style.position = 'relative'
+  document.body.append(container)
+  const labeledFaceDescriptors = await loadLabeledImages()
+  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
+  let image
+  let canvas
+  document.body.append('Loaded')
+  imageUpload.addEventListener('change', async () => {
+    if (image) image.remove()
+    if (canvas) canvas.remove()
+    image = await faceapi.bufferToImage(imageUpload.files[0])
+    container.append(image)
+    canvas = faceapi.createCanvasFromMedia(image)
+    container.append(canvas)
+    const displaySize = { width: image.width, height: image.height }
+    faceapi.matchDimensions(canvas, displaySize)
+    const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors()
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
+    results.forEach((result, i) => {
+      console.log(result.toString())
+      const box = resizedDetections[i].detection.box
+      const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
+      drawBox.draw(canvas)
+    })
+    container.append(image)
+    container.append(canvas)
+    const detections2 = await faceapi.detectAllFaces(image, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+    const resizedDetections2 = faceapi.resizeResults(detections2, displaySize)
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    faceapi.draw.drawDetections(canvas, resizedDetections2)
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections2)
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections2)
+    console.log(faceapi.FaceExpressions(canvas, resizedDetections2))
+  })
+}
+
+function loadLabeledImages() {
+  const labels = ['Black Widow', 'Captain America', 'Captain Marvel', 'Hawkeye', 'Jim Rhodes', 'Thor', 'Tony Stark','Ruhul']
+  return Promise.all(
+    labels.map(async label => {
+      const descriptions = []
+      for (let i = 1; i <= 2; i++) {
+        const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/ruhul0/Face-Detection/master/labeled_images/${label}/${i}.jpg`)
+        const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+        descriptions.push(detections.descriptor)
+      }
+
+      return new faceapi.LabeledFaceDescriptors(label, descriptions)
+    })
+  )
+}
